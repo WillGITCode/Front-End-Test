ebbfdef056f743bb13b3033c6f36498e522982ea
William S <25758160+WillGITCode@users.noreply.github.com>
2018-04-24 22:54:09 -0400

    added bootstrap, partials/header and footer, main.css

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 const express = require('express')
 const app = express()
+//Installed node-fetch because fetch didn't work in node enviroment
+//TODO find out why 
 const fetch = require('node-fetch');
+
+app.use(express.static("public"));
 
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-    fetch('https://jsonplaceholder.typicode.com/posts/1')
-    .then(response => response.json())
-    .then(json => console.log(json))
+    
     res.render('landing')
 })
 
-app.get('/showpage', (req, res) => res.render('showpage'))
+app.get('/showpage', (req, res) => {
+    const posts = fetch('https://jsonplaceholder.typicode.com/posts/1')
+    .then(response => response.json())
+    .then(json => console.log(json))
+    res.render('showpage', {posts:posts})
+})
 
 app.listen(3000, () => console.log('Font-End Test Server Started!'))
