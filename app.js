fcc00718cd1bb08177fbdd45440201ac03332bca
William S <25758160+WillGITCode@users.noreply.github.com>
2018-04-26 00:05:39 -0400

    pass data to showpage, start using json placeholder api

--- app.js
+++ app.js
@@ -13,11 +13,36 @@ app.get('/', (req, res) => {
     res.render('landing')
 })
 
+let posts = [
+    {id: 99,
+     title: 'ja;fkhjglhlk jlkjalffhjggl   alfghlj',
+     thumb: 'urljlajgfg'   
+    },
+    {
+    id: 99,
+    title: 'nnnnnns   sgj s ',
+    thumb: 'fjjjjjgfg' 
+    },
+    {
+    id: 99,
+     title: '5645     45664         457464',
+     thumb: 'fhj hjsfghjs'
+    }]
+
+// var posts =[];
+
+fetch('https://jsonplaceholder.typicode.com/posts')
+.then(response => response.json()
+.then(data => posts = data))
+
+
 app.get('/showpage', (req, res) => {
-    const posts = fetch('https://jsonplaceholder.typicode.com/posts/1')
-    .then(response => response.json())
-    .then(json => console.log(json))
+    
+
     res.render('showpage', {posts:posts})
 })
+
+
+
 
 app.listen(3000, () => console.log('Font-End Test Server Started!'))
