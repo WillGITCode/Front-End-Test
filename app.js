f27573875ca9493ea653170b19394535a247e55c
William S <25758160+WillGITCode@users.noreply.github.com>
2018-04-26 07:35:57 -0400

    showpage loop through obj from DB showing post titles

--- app.js
+++ app.js
@@ -8,33 +8,39 @@ app.use(express.static("public"));
 
 app.set('view engine', 'ejs')
 
-app.get('/', (req, res) => {
-    
-    res.render('landing')
-})
 
-let posts = [
-    {id: 99,
-     title: 'ja;fkhjglhlk jlkjalffhjggl   alfghlj',
-     thumb: 'urljlajgfg'   
-    },
-    {
-    id: 99,
-    title: 'nnnnnns   sgj s ',
-    thumb: 'fjjjjjgfg' 
-    },
-    {
-    id: 99,
-     title: '5645     45664         457464',
-     thumb: 'fhj hjsfghjs'
-    }]
+//Set up temp server data using api
+//example Shcema
 
-// var posts =[];
+// let posts = [
+//     {id: 99,
+//      title: 'ja;fkhjglhlk jlkjalffhjggl   alfghlj',
+//      thumb: 'urljlajgfg'   
+//     },
+//     {
+//     id: 99,
+//     title: 'nnnnnns   sgj s ',
+//     thumb: 'fjjjjjgfg' 
+//     },
+//     {
+//     id: 99,
+//      title: '5645     45664         457464',
+//      thumb: 'fhj hjsfghjs'
+//     }]
+
+let posts =[];
 
 fetch('https://jsonplaceholder.typicode.com/posts')
 .then(response => response.json()
 .then(data => posts = data))
 
+
+
+
+app.get('/', (req, res) => {
+    
+    res.render('landing')
+})
 
 app.get('/showpage', (req, res) => {
     
