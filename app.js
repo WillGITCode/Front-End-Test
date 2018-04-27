b003771f80720a49a9b347d8e6d4589646faa418
William S <25758160+WillGITCode@users.noreply.github.com>
2018-04-26 20:30:24 -0400

    show page displays posts, restful routs begun, new post page passes data back to DB

--- app.js
+++ app.js
@@ -1,51 +1,65 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 //Installed node-fetch because fetch didn't work in node enviroment
 //TODO find out why 
 const fetch = require('node-fetch');
 
 app.use(express.static("public"));
-
+app.use(bodyParser.urlencoded({extended: true}))
 app.set('view engine', 'ejs')
 
 
 //Set up temp server data using api
 //example Shcema
 
-// let posts = [
-//     {id: 99,
+// let posts = [{
+//      userId: 1,
+//      id: 99,
 //      title: 'ja;fkhjglhlk jlkjalffhjggl   alfghlj',
-//      thumb: 'urljlajgfg'   
+//      body: 'urljlajgfg jpadfjgljfg ljlfkjg jlkjlfgjlkj'   
 //     },
 //     {
-//     id: 99,
-//     title: 'nnnnnns   sgj s ',
-//     thumb: 'fjjjjjgfg' 
-//     },
-//     {
-//     id: 99,
+//      userId: 1,    
+//      id: 99,
 //      title: '5645     45664         457464',
-//      thumb: 'fhj hjsfghjs'
+//      body: 'fhj hjsfgh jpadfjgljfg ljlfkjg jlkjlfgjlkjjs'
 //     }]
 
-let posts =[];
+let users,
+    posts,
+    albums,
+    photos,
+    comments;
 
 fetch('https://jsonplaceholder.typicode.com/posts')
 .then(response => response.json()
 .then(data => posts = data))
 
-
-
-
+//Landing rout
 app.get('/', (req, res) => {
-    
     res.render('landing')
 })
 
-app.get('/showpage', (req, res) => {
-    
+//Show posts rout
+app.get('/posts', (req, res) => {
+    res.render('posts', {posts:posts})
+})
 
-    res.render('showpage', {posts:posts})
+//New Post post rout
+app.post('/posts', (req, res) => {
+    //get data from form
+    let title = req.body.title
+    let body = req.body.body
+    let newPost = {title: title, body: body}
+    posts.push(newPost)
+    //redirect to posts page
+    res.redirect('/posts')
+})
+
+//New Post form rout
+app.get('/posts/new', (req, res) => {
+    res.render('new.ejs')
 })
 
 
